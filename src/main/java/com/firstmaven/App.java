3774005145d10ba7a6ffde9b1324958c75fdb33d
KateM3d <ekaterina8medvedeva@gmail.com>
2023-02-25 10:37:57 -0500

    first commit

---
+++ src/main/java/com/firstmaven/App.java
@@ -0,0 +1,10 @@
+package com.firstmaven;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
